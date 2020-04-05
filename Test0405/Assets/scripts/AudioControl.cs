890edbfcacb152ed4153300e86d1e3be9e7a934c
sheny <973785296@qq.com>
2020-04-05 17:16:36 +0800

    update

---
+++ Test0405/Assets/scripts/AudioControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AudioControl : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ButtonPlayClickFun()
+    {
+        audioSource.Play();
+    }
+
+    public void ButtonPauseClickFun()
+    {
+        audioSource.Pause();
+    }
+}
---
+++ Test0405/Assets/scripts/MixAudioControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MixAudioControl : MonoBehaviour
+{
+    public AudioMixer AudioMixer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MasterChange(Slider slider)
+    {
+        AudioMixer.SetFloat("MasterParam", slider.value);
+    }
+
+    public void Music1Change(Slider slider)
+    {
+        AudioMixer.SetFloat("Music2Param", slider.value);
+    }
+
+    public void Music2Change(Slider slider)
+    {
+        AudioMixer.SetFloat("Music3Param", slider.value);
+    }
+}
